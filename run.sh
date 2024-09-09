3295ba3f28487461ec959438b9a6a5e87db0f855
Guryanov David <DDGuryanov@alfabank.ru>
2024-09-09 11:41:04 +0300

    на глазок2

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd tests-selfmade-project-6-public/
+cd tests-selfmade-project-6-forked/
 npm i
 npm i -g wait-port http-server
 http-server ../ -p 3000 & wait-port -t 30000 localhost:3000
